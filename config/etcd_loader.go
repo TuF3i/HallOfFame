ccadff2b04c7c9bdda3b49a645f19b802dfbdf5c
TuF3i <13759301652@163.com>
2026-06-05 17:53:24 +0800

    ✨ 完成ConfigLodaer

--- config/etcd_loader.go
+++ config/etcd_loader.go
@@ -1,26 +1,16 @@
 package config
 
 import (
+	"HallOfFame/internal/etcd"
 	"context"
 	"encoding/json"
 	"fmt"
-
-	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
-// LoadFromEtcd 从 etcd 加载完整配置。
-// etcd 中 /halloffame/config 这个 key 的值就是整个配置的 JSON。
-func LoadFromEtcd(ctx context.Context, addr string, port int) (*Config, error) {
-	client, err := clientv3.New(clientv3.Config{
-		Endpoints: []string{fmt.Sprintf("%s:%d", addr, port)},
-	})
-	if err != nil {
-		return nil, fmt.Errorf("create etcd client: %w", err)
-	}
-	defer client.Close()
+func LoadFromEtcd(ctx context.Context, client *etcd.EtcdClient) (*Config, error) {
 
 	key := "/halloffame/config"
-	resp, err := client.Get(ctx, key)
+	resp, err := client.Client.Get(ctx, key)
 	if err != nil {
 		return nil, fmt.Errorf("etcd get key %q: %w", key, err)
 	}
